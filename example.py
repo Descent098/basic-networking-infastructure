10bba1a813b86654cd3703002fb3e137d8fd2776
Descent098 <Descent098@users.noreply.github.com>
2022-07-01 07:25:36 +0000

    deploy: 0a82e38eefbe13f63984eb509889670b19533fa3

--- example.py
+++ example.py
@@ -12,49 +12,59 @@ Slide("A more accurate view", background="red") # Infastructure is not part of t
 Slide("A more accurate view example", background="red") # Person requests service, server then processes request and responds with the frontend which once it's gone through all the infastructure gets displayed
 Slide("So how does infastructure work?", "For this presentation we don't care about the frontend or backend. As far as we're concerned there's a working server with all the webpages built and we're just hooking it up so people can access it outside the developers computer",background="red")
 
-# Terminology TODO
+# Terminology
 Slide("Let's start with some terms")
 Slide("What is a browser?", "A browser lets you connect to a server over a network and then visualizes the response it recieves back (HTML/CSS/JS, images, pdf's etc.)") 
 Slide("What is a network?", "A network is a collection of computers that are connected together and can communicate with one another. Generally speaking the internet is a massive network of many computers connected together") 
 Slide("What is a server/host vs client?", "The server or host is the computer that is SENDING responses to the client (i.e. when you go to google, the computer that gives you the page is the server and google is the host)", "The client is whatever is RECIEVING the responses from the server (i.e. the person trying to access google and their browser)", "Technically speaking both groups do both actions, but overall the goal of the client is to recieve from the server and overall the goal of the server is to send to the client")
 
-## URL Explanations TODO
+## URL Explanations
 Slide("What is a URL?", "A URL is what you type in a browser to get a webpage, for example https://schulichignite.com/beginner it has multiple parts and follows the form:", Code("", "https://schulichignite.com/beginner\n$Protocol://$domain.$tld/$slug"), r"*Anything in angle brackets or starting with a $ is a variable")
-Slide("What is a slug?", "The slug is the bit at the end, so in https://schulichignite.com/beginner the /beginner is the slug. This is used to specify what you are looking for on the server. One site can have many pages so a slug is how the server knows what to look for", "Slugs used to just be file paths, so for example you would have https://schulichignite.com/beginner.html, which would just look for the beginner.html file")
-Slide("What is a tld?")
-Slide("What is a protocol?", "We'll come back to this in a bit")
-Slide("What is a domain?") #TODO: Explain FQDN
+Slide("What is a slug?", "The slug is the bit at the end, so in https://schulichignite.com/beginner the /beginner is the slug. This is used to specify what you are looking for on the server. One site can have many pages so a slug is how the server knows what to look for", "Slugs used to just be file paths, so for example you would have https://schulichignite.com/beginner.html, which the server would just look for the beginner.html file and send it")
+Slide("What is a tld?", "The TLD (Top level domain) is the .com in https://schulichignite.com/beginner. The difference is that some TLD's have rules, for example .ca domains require you to provide your address and name, and the .io domain requires certain security standards etc.")
+Slide("What is a protocol?", "We'll come back to this in a bit, on the web this is basically always http:// or https://, if you're loading a file it will be file://$filepath")
+Slide("What is a domain?", "A domain is essentially a name you purchase. You buy a domain name (which includes A tld not all tld's), and that indicates you own it and can do what you want with it. For example schulichignite.com (this is also sometimes called a FQDN (fully qualified domain name)")
 
 
 # Domain Name registrar
 Slide("How does your browser know who owns a domain?", "This is where a domain name registrar comes in. You pay them to validate that you own a domaain")
-Slide("Common Domain Name Registrars", "These are a dime a dozen but in Canada I would recommend either GoDaddy, webnames or namecheap")
+Slide("Common Domain Name Registrars", "These are a dime a dozen but in Canada I would recommend either GoDaddy, webnames or namecheap (remember you have to renew your ownership or other people can take your domain)")
 Slide("Great so we own the domain, but how do people talk to it?","Well they need to request a URL, and we need to give them a response")
 
-# HTTP TODO 
+# HTTP 
 Slide("HTTP", "hyper-text transfer protocol; This is what defines how two computers structure their data to talk to each other (it's the http or https at the beginning of a URL)", background="black-blue")
-Slide("HTTP Diagram", "Imagine bobby wants to get the shulich ignite beginner sessions page (https://schulichignite.com/beginner)",  background="black-blue") # TODO Image; Client --> HTTP Request --> Server --> HTTP response --> Client
+Slide("HTTP Diagram", "Imagine bobby wants to get the shulich ignite beginner sessions page (https://schulichignite.com/beginner)", Image("bobby-http", "bobby-http.png"), "This whole interaction and how it works is defined by the HTTP protocol",  background="black-blue")
 
 ## Headers TODO
-Slide("HTTP Headers", background="black-blue")
-Slide("Mail analogy", "Imagine you're sending mail, you need 3 things", ["Your address", "Their Address", "The content of the letter"] , "That is what HTTP needs", ["Address the communication was sent from", "Address it's going to", "The Content (text, html, image etc.)"],  background="black-blue") # TODO
+Slide("HTTP Headers","HTTP requests and responses both have headers. Headers are key-value pairs that tell the client & host the details they need to interact properly", background="black-blue")
+Slide("Mail analogy", "Imagine you're sending mail, you need 3 things", ["Your address", "Their Address", "The content of the letter"] ,"", "That is what HTTP needs", ["Address the communication was sent from", "Address it's going to", "The Content (text, html, image etc.)"],"", "Additionally like how you would add FRAGILE to mail to tell the post service how to handle your message, headers can also contain information about how to handle the request/response", background="black-blue")
 Slide("How to examine headers in browser", background="black-blue") #TODO: Show in browser how you can inspect HTTP headers
 
-## Requests TODO
+## Requests
+Slide("HTTP Requests", "The first part of HTTP communication is to request a URL", background="black-blue")
+Slide("HTTP Request Example", "We're going to request https://schulichignite.com/beginner",Code("","GET /beginner HTTP/1.1\nHost: schulichignite.com\nUser-Agent:... Removed for readability"), background="black-blue")
 Slide("HTTP Request types (GET/HEAD)", "A GET request asks for a response that includes a header and some content, a HEAD request asks for just a header response without any content",background="black-blue") # GET
 Slide("HTTP Request types (POST)", "A POST request POSTs some content to a server. This is usually done for forms to submit the data",background="black-blue") # POST
 Slide("HTTP Request types (PUT)", "A PUT request tells the server to PUT whatever is being sent at the location specified. This might be used on a photo sharing service to indicate replacing a photo with a new version.",background="black-blue") # PUT
 Slide("HTTP Request types (DELETE)","A DELETE request is used to ask a server to DELETE a resource (i.e. a photo)", background="black-blue") # DELETE
 Slide("Other HTTP Request types", Link("This link contains more request types", "https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods"), background="black-blue") # DELETE
-Slide("HTTP Request Example", "We're going to request https://schulichignite.com/beginner", background="black-blue") #TODO: Example
 Slide("Breaking down the headers", "This won't cover everything, just the things that are useful", background="black-blue")
-Slide("First line","The first line tells the browser the request type, slug, protocol and version", Code("bash", r"GET /hello-world HTTP/1.1"), background="black-blue") #TODO
+Slide("First line","The first line tells the browser the request type, slug, protocol and version", Code("bash", r"GET /beginner HTTP/1.1"), background="black-blue")
 Slide("Host", "This tells the server & browser the domain you're trying to access", Code("","Host: schulichignite.com"), background="black-blue")
-Slide("User Agent", "This tells the server information about your browser. This is used by developers often to patch compatability if a browser doesn't support a feature (usually REALLY long)", Code("","User-Agent: <browser user agent> "), background="black-blue")
+Slide("User Agent", "This tells the server information about your browser. This is used by developers often to patch compatability if a browser doesn't support a feature (usually REALLY long)", Code("","User-Agent: Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.53 Mobile Safari/537.36 Edg/103.0.1264.37 "), background="black-blue")
 
 ## Responses
-Slide("HTTP Response", "The response is comprised of a header, and the response content (the HTML or file contents of the resource)",background="black-blue")# TODO: example
-Slide("HTTP Response header example", background="black-blue") #TODO: example
+
+response_example = '''<pre><code class="language-html hljs xml">HTTP/1.x 200 OK
+Content-Type: text/html; charset=UTF-8
+Cache-Control: max-age=3600, public
+Content-Encoding: gzip
+
+&lt;title&gt; Schulich Ignite &lt;/title&gt;&lt;!--More HTML here--&gt;
+</code></pre>
+'''
+Slide("HTTP Response", "The response is comprised of a header, and the response content (the HTML or file contents of the resource) Example", Raw(response_example),background="black-blue")
+Slide("HTTP Response header example", "Like requests the response headers provide additional details. Usually about the type of content being sent and other information",Code("","HTTP/1.x 200 OK\nContent-Type: text/html; charset=UTF-8\nCache-Control: max-age=3600, public\nContent-Encoding: gzip"), background="black-blue")
 Slide("Breaking down HTTP Response header", "Again this isn't everything, just things that are often useful", background="black-blue")
 Slide("First line", "First line is always the response protocol, version and status code", Code("","HTTP/1.x 200 OK"), background="black-blue")
 Slide("HTTP Response status codes 2xx", "These codes means everything is working as intended. Examples:",Code("","200 OK; Successful response\n201 Created; A new file was created successfully\n202 Accepted; The request has been accepted, but isn't done yet"), background="black-blue")
@@ -69,24 +79,24 @@ Slide("Great, so we know about domains & HTTP let's go", "So we should be able t
 
 
 # Domain Name System TODO
-Slide("Public IP addresses and ports", "HTTP connects to the PUBLIC IP address and port of a computer, not the domain!", "Let's say that a computer is like an apartment building. You can think of the IP address as the coordinates to the building (computer), and the port as which apartment to go to in the building (computer)", background="green")
+Slide("Public IP addresses and ports", "HTTP connects to the PUBLIC IP address and port of a computer, not the domain!", "Let's say that a computer is like an apartment building. You can think of the IP address as the coordinates to the building (computer), and the port as which apartment to go to in the building (computer)", Code("","192.0. 2.146:80\n[2001:0db8:85a3:0000:0000:8a2e:0370:7334]:443"),background="green")
 Slide("How does a browser know a server's PUBLIC IP address and port based off the domain?", "The domain name registrar tells us who owns it, but once you own the domain you need to configure it. That's where DNS comes in!", background="green") # TODO: image
 Slide("But wait, there's another server involved", "Nameservers are the in-between step where your domain name registrar tells your browser where to go to find your DNS server and records", background="green") # TODO: image 
 Slide("So to recap at this point", "We have made a request to a domain, we then check with the domain name registrar for the nameservers to find the DNS. Now we need to know how the DNS works", background="green")
 
-## DNS records TODO
-Slide("DNS is made up of records", "Your domain is used for lots of things, so DNS has lots of different types of records to handle those situations. You can think of records like a contact list. It lets you look up where to communicate with a server.", background="green")
-Slide("Common DNS Record types", background="green")
-Slide("Alias records", background="green")
-Slide("Canonical Name Records", background="green")
-Slide("MX records", background="green")
-Slide("TXT Records", background="green")
-Slide("NS Records", background="green")
+## DNS records
+Slide("DNS is made up of records", "Your domain is used for lots of things, so DNS has lots of different types of records to handle those situations. You can think of records like a contact list. It lets you look up where to communicate with a server.",Image("kieranwood-dns","kieranwood-dns.png"), background="green")
+Slide("Common DNS Record types", "Here are the most common DNS records you should know about. Please note the records are listed in the following format",Code("","$record_type $domain_name $content"), "also keep in mind @ in place of domain name is self-referential (i.e. using @ for dns of schulichignite.com would be the same as typing schulichignite.com)",background="green")
+Slide("Alias records (A or AAAA)", "There are two types of alias records A or AAAA, these map a domain to an IP. A records for IPV4 adresses and AAAA for IPV6", "These are the primary records used to tell the browser which server to look for to get a site", Code("","A @ 185.199.111.153\nAAAA @ 2001:0db8:85a3:0000:0000:8a2e:0370:7334"),background="green")
+Slide("Canonical Name Records (CNAME)", "These are used to set a domain to another domain. For example this would allow you to have www.schulichignite.com which maps to the same content as schulichignite.com, or spark.schulichignite.com map to the content of schulich-ignite.github.io", Code("","CNAME spark schulich-ignite.github.io"),background="green")
+Slide("Mail Exchanger Record (MX)", "These are records used to setup a SMTP (email) server",Code("","MX shuclichignite.com mail.domain.com"),background="green")
+Slide("TXT Records", "These are just plain text, they are often used to verify to third party services (like google) that you own a domain", Code("","TXT @ $verification_code"), background="green")
+Slide("NS Records", "These records tell you which nameserver is used for a domain", Code("","NS @ $nameserver_domain"),background="green")
 Slide("The rest", Link("Wikipedia page of all DNS records", "https://en.wikipedia.org/wiki/List_of_DNS_record_types"), background="green")
 
-## DNS Providors & validation TODO
-Slide("DNS providors", background="green") #TODO Cloudflare, DNR
-Slide("Checking DNS records", "There are some online tools:", Link("dnschecker", "https://dnschecker.org/"),"linux also includes a tool typically called dig, you can install this on windows by installing BIND", "I also wrote a python tool you can use:", Link("sws", "https://github.com/descent098/sws"), background="green")# TODO: Add tools to check DNS records
+## DNS Providors & validation
+Slide("DNS providors", "Sometimes your domain name registrar will provide DNS. It is usually better to go with a dedicated DNS, I highly recommend", Link("cloudflare","https://www.cloudflare.com/", color="#eeeeeee"),background="green")
+Slide("Checking DNS records", "There are some online tools:", Link("dnschecker", "https://dnschecker.org/"),"linux also includes a tool typically called dig, you can install this on windows by installing BIND", "I also wrote a python tool you can use:", Link("sws", "https://github.com/descent098/sws"), background="green")
 
 
 # Putting it all together TODO
@@ -124,6 +134,9 @@ Slide("Some handy tools/commands you can use", background="white")
 Slide("(ifconfig/ip)/ipconfig", background="white") #TODO
 Slide("ping", "ping can be used to check if a server is running you can use a domain or IP", Code("bash", "ping shulichignite.com\nping 8.8.8.8"),background="white") # TODO
 Slide("netcat/telnet","Netcat/telnet are used for many things, but one thing both can be used for is connecting to a server and sending requests and getting responses (example is just connecting)", "linux", "nc -l port", "windows", Code("bash", "telnet hostname port"), background="white") # TODO
+
+## Great Channel
+Slide("A great channel for networking info", Link("Hussein Nasser", "https://www.youtube.com/c/HusseinNasser-software-engineering"), background="white")
 
 # Setup the presentation
 presentation_title = "Basic Networking & Infastructure"
