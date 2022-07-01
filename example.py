179a81a45993bd3c4e02f0dd8dd7de6734d481a4
Descent098 <Descent098@users.noreply.github.com>
2022-07-01 23:54:28 +0000

    deploy: 8e9617fb007cf4ce1123396ad9fb1fd733629f54

--- example.py
+++ example.py
@@ -1,15 +1,21 @@
 from ezprez.core import *
 from ezprez.components import *
 
-# Structure of a web app TODO
+# Structure of a web app
 Slide("Structure of a web app", background="red")
-Slide("Backend", background="red")
-Slide("Middlewear", background="red")
-Slide("Front end", background="red")
-Slide("Traditional view", background="red")
-Slide("Traditional view example", background="red") # Person requests service, server then processes request and responds with the frontend
-Slide("A more accurate view", background="red") # Infastructure is not part of the backend, it is it's own system
-Slide("A more accurate view example", background="red") # Person requests service, server then processes request and responds with the frontend which once it's gone through all the infastructure gets displayed
+
+## Traditional view
+Slide("Traditional Structure", "There are two parts of an app, the front end and the back end", Image("traditional-view", "traditional-view.png"), background="red")
+Slide("Front end", "This is the part people see, in a web app it's the HTML/CSS/JS that people actually see in their browsers when they go to your site", background="red")
+Slide("Backend", "This is the name given to everything else. Everything that does data processing and delivering of the front-end and any data the front end needs",background="red")
+Slide("Traditional Structure example", "User requests webpage --> Frontend is delievered through backend --> Frontend gets data from backend and shows user page",background="red") # Person requests service, server then processes request and responds with the frontend
+Slide("The problem with this view", "The backend has way too many peices in this view, and it's responsible for multiple jobs. If you want to talk about an app saying it's a problem with the'backend' is not helpful information", background="red")
+
+## More accuratie view
+Slide("A more accurate Structure", "A more accurate structure is that there's a front end, a back end and then infastructure in-between the client and the app/site", Image("better-view", "better-view.png"), background="red") # Infastructure is not part of the backend, it is it's own system
+Slide("Infastructure", "Infastructure is the name given to the peices that deliver the data to a user. It's also the name for peices that deliver the data from the frontend to the backend, but aren't involved in any of the processing.", background='red')
+Slide("Why Split out infastructure?", "This makes it a bit easier to intuitively tell which peices go where, and allows you to isolate which part of an app/site you're talking about more easily, for example this means if the program running an app/site is running, but people outside the building can't see it it's an infastructure problem, not a backend problem.", background="red")
+Slide("A more accurate Structure example", "User requests a webpage --> Goes through infastructure to retrieve front-end --> Front end goes back to infastructure to get data from backend --> User see's page with all content",background="red") # Person requests service, server then processes request and responds with the frontend which once it's gone through all the infastructure gets displayed
 Slide("So how does infastructure work?", "For this presentation we don't care about the frontend or backend. As far as we're concerned there's a working server with all the webpages built and we're just hooking it up so people can access it outside the developers computer",background="red")
 
 # Terminology
@@ -35,10 +41,10 @@ Slide("Great so we own the domain, but how do people talk to it?","Well they nee
 Slide("HTTP", "hyper-text transfer protocol; This is what defines how two computers structure their data to talk to each other (it's the http or https at the beginning of a URL)", background="black-blue")
 Slide("HTTP Diagram", "Imagine bobby wants to get the shulich ignite beginner sessions page (https://schulichignite.com/beginner)", Image("bobby-http", "bobby-http.png"), "This whole interaction and how it works is defined by the HTTP protocol",  background="black-blue")
 
-## Headers TODO
+## Headers
 Slide("HTTP Headers","HTTP requests and responses both have headers. Headers are key-value pairs that tell the client & host the details they need to interact properly", background="black-blue")
 Slide("Mail analogy", "Imagine you're sending mail, you need 3 things", ["Your address", "Their Address", "The content of the letter"] ,"", "That is what HTTP needs", ["Address the communication was sent from", "Address it's going to", "The Content (text, html, image etc.)"],"", "Additionally like how you would add FRAGILE to mail to tell the post service how to handle your message, headers can also contain information about how to handle the request/response", background="black-blue")
-Slide("How to examine headers in browser", background="black-blue") #TODO: Show in browser how you can inspect HTTP headers
+Slide("How to examine headers in browser", "First open your browser dev tools, go to the network tab and then either refresh or navigate to a webpage", Image("view-headers","view-headers.png"),background="black-blue")
 
 ## Requests
 Slide("HTTP Requests", "The first part of HTTP communication is to request a URL", background="black-blue")
@@ -78,10 +84,10 @@ Slide("Content Encoding", "Web pages aren't usually sent as plaintext that is hu
 Slide("Great, so we know about domains & HTTP let's go", "So we should be able to just connect to the domain and send HTTP requests and get responses, right?", background="black-blue")
 
 
-# Domain Name System TODO
+# Domain Name System
 Slide("Public IP addresses and ports", "HTTP connects to the PUBLIC IP address and port of a computer, not the domain!", "Let's say that a computer is like an apartment building. You can think of the IP address as the coordinates to the building (computer), and the port as which apartment to go to in the building (computer)", Code("","192.0. 2.146:80\n[2001:0db8:85a3:0000:0000:8a2e:0370:7334]:443"),background="green")
-Slide("How does a browser know a server's PUBLIC IP address and port based off the domain?", "The domain name registrar tells us who owns it, but once you own the domain you need to configure it. That's where DNS comes in!", background="green") # TODO: image
-Slide("But wait, there's another server involved", "Nameservers are the in-between step where your domain name registrar tells your browser where to go to find your DNS server and records", background="green") # TODO: image 
+Slide("How does a browser know a server's PUBLIC IP address and port based off the domain?", "The domain name registrar tells us who owns it, but once you own the domain you need to configure it. That's where DNS comes in!", background="green")
+Slide("But wait, there's another server involved", "Nameservers are the in-between step where your domain name registrar tells your browser where to go to find your DNS server and records", Image("ns", "ns.png"),background="green") 
 Slide("So to recap at this point", "We have made a request to a domain, we then check with the domain name registrar for the nameservers to find the DNS. Now we need to know how the DNS works", background="green")
 
 ## DNS records
@@ -99,14 +105,14 @@ Slide("DNS providors", "Sometimes your domain name registrar will provide DNS. I
 Slide("Checking DNS records", "There are some online tools:", Link("dnschecker", "https://dnschecker.org/"),"linux also includes a tool typically called dig, you can install this on windows by installing BIND", "I also wrote a python tool you can use:", Link("sws", "https://github.com/descent098/sws"), background="green")
 
 
-# Putting it all together TODO
+# Putting it all together
 Slide("Putting it all together", "Let's go through a full example one step at a time", background="purple")
-Slide("Step 1", "Someone types https://schulichignite.com/beginner/ into their browser and hit's enter", background="purple")
+Slide("Step 1", "Someone types https://schulichignite.com/beginner/ into their browser and hit's enter", Image("step 1", "step-1.png"),background="purple")
 Slide("Step 2", "Browser pings the domain name registrar the domain was bought with and determines the nameservers", background="purple")
-Slide("Step 3", "Now the browser has the nameservers, since we want the site we're looking for an A, AAAA or CNAME record (in this case it's an A record pointing to github pages)", background="purple")
-Slide("Step 4", "The HTTP request is then passed to the IP address and port listed", background="purple")
-Slide("Step 5", "The site is hosted with github pages, which recieves the request and looks for the corresponding HTML page for the /beginner slug", background="purple")
-Slide("Step 6", "The browser recieves an HTTP response with a 200 status code that has the content of the webpage requested (additional HTTP requests will be made for assets in the HTML file like images and external CSS files)", background="purple")
+Slide("Step 3", "Now the browser has the nameservers, since we want the site we're looking for an A, AAAA or CNAME record (in this case it's an A record pointing to github pages)", Image("step 3", "step-3.png"), background="purple")
+Slide("Step 4", "The HTTP request is then passed to the IP address and port listed", Image("step 4", "step-4.png"),background="purple")
+Slide("Step 5", "The site is hosted with github pages, which recieves the request and looks for the corresponding HTML page for the /beginner slug", Image("step 4", "step-4.png"), background="purple")
+Slide("Step 6", "The browser recieves an HTTP response with a 200 status code that has the content of the webpage requested (additional HTTP requests will be made for assets in the HTML file like images and external CSS files)", Image("step 6", "step-6.png"), background="purple")
 
 
 # Extra stuff
@@ -129,11 +135,11 @@ Slide("Bind the socket", "Binding a socket means you set the INTERNAL IP address
 Slide("Ask the socket to listen", "Does what it says on the tin, it sets the server to wait for a request to come in (HTTP Requests in our case)", Link("Code example (also line 38)", "https://github.com/Descent098/ezcv-http/blob/main/socket%20testing/routes.py#L26"), background="white")
 Slide("Process client request and send response", "Take in the client request, determine the content and headers you want to send back", Link("Code example", "https://github.com/Descent098/ezcv-http/blob/main/socket%20testing/routes.py#L40-L118"), background="white")
 
-## Handy CLI tools TODO 
+## Handy CLI tools 
 Slide("Some handy tools/commands you can use", background="white")
-Slide("(ifconfig/ip)/ipconfig", background="white") #TODO
-Slide("ping", "ping can be used to check if a server is running you can use a domain or IP", Code("bash", "ping shulichignite.com\nping 8.8.8.8"),background="white") # TODO
-Slide("netcat/telnet","Netcat/telnet are used for many things, but one thing both can be used for is connecting to a server and sending requests and getting responses (example is just connecting)", "linux", "nc -l port", "windows", Code("bash", "telnet hostname port"), background="white") # TODO
+Slide("(ifconfig/ip)/ipconfig", "Gives you the information about the LOCAL IP of your machine (for PUBLIC IP you need to google it or have it provided)", background="white")
+Slide("ping", "ping can be used to check if a server is running you can use a domain or IP", Code("bash", "ping shulichignite.com\nping 8.8.8.8"),background="white")
+Slide("netcat/telnet","Netcat/telnet are used for many things, but one thing both can be used for is connecting to a server and sending requests and getting responses (example is just connecting)", "linux", "nc -l port", "windows", Code("bash", "telnet hostname port"), background="white")
 
 ## Great Channel
 Slide("A great channel for networking info", Link("Hussein Nasser", "https://www.youtube.com/c/HusseinNasser-software-engineering"), background="white")
